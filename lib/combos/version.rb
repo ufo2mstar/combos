45f571ff98c3df23796b5285e33616e28637b2ed
Naren SivaSubramani <ufo2mstar@gmail.com>
2016-12-21 18:06:19 -0500

    SPEC: modularizing the spec info into constants

--- lib/combos/version.rb
+++ lib/combos/version.rb
@@ -1,3 +1,20 @@
 module Combos
   VERSION = "0.1.0"
+
+  SPEC_SUMMARY = <<EOF
+%q{TODO: Write a short summary, because Rubygems requires one.}
+EOF
+
+  SPEC_DESCRIPTION = <<EOF
+      %q{TODO: Write a longer description or delete this line.}
+EOF
+
+  SPEC_HOMEPAGE = <<EOF
+      "todo: put your gem's website or public repo url here."
+EOF
+
+  SPEC_METADATA = <<EOF
+      "TODO: Set to 'http://mygemserver.com'"
+EOF
+
 end
